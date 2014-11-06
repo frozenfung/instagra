7f3135079a55168d636a6d575a1e1a0a76709017
Reinhardt Lin <reinhardt.lin@gmail.com>
2014-11-06 16:36:39 +0800

    add facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  get '/auth/:provider/callback', :to => 'sessions#create'
+  get '/auth/failure', to: 'sessions#failure'
+  delete '/auth/signout', to: 'sessions#destroy'
+
+  root 'welcome#index'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
